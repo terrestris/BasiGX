da610f5bc56e0c3e922fce31415610182476fcdd
Marc Jansen <jansen@terrestris.de>
2015-10-27 14:22:53 +0000

    Add basic test setup

---
+++ test/load-tests.js
@@ -0,0 +1,15 @@
+/*global document*/
+// This file is taken from GeoExt3
+(function(doc, global){
+    var specPath = './spec/',
+        dependencies = [
+            'basics.js'
+        ],
+        getScriptTag = global.TestUtil.getExternalScriptTag,
+        dependencyCnt = dependencies.length,
+        i = 0;
+
+    for(; i < dependencyCnt; i++) {
+        doc.write(getScriptTag(specPath + dependencies[i]));
+    }
+}(document, this));
