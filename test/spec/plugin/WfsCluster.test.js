881adbf39b87c44e0f8c028d3ddbc601b555619e
Johannes Weskamm <weskamm@terrestris.de>
2015-11-05 15:48:16 +0100

    added tests for wfscluster, animate, application and configparser

--- test/spec/plugin/WfsCluster.test.js
+++ test/spec/plugin/WfsCluster.test.js
@@ -5,5 +5,82 @@ describe('BasiGX.plugin.WfsCluster', function() {
         it('is defined', function() {
             expect(BasiGX.plugin.WfsCluster).to.not.be(undefined);
         });
+        it('can be instantiated', function(){
+            var plugin = Ext.create('BasiGX.plugin.WfsCluster');
+            expect(plugin).to.be.a(BasiGX.plugin.WfsCluster);
+        });
+    });
+    describe('Usage as plugin for BasiGX.view.component.Map', function() {
+        var plugin;
+        var mapComponent;
+        var div;
+        var clusterLayerConf;
+        var layer;
+        beforeEach(function() {
+            div = document.createElement('div');
+            document.body.appendChild(div);
+            clusterLayerConf = {
+                type: 'WFSCluster',
+                layers: 'test',
+                url: 'http://test.com'
+            };
+            mapComponent = Ext.create('BasiGX.view.component.Map', {
+                plugins: ['wfscluster'],
+                map: new ol.Map({
+                    target: div,
+                    projection: 'EPSG:3857'
+                })
+            });
+            BasiGX.util.ConfigParser.map = mapComponent.getMap();
+            layer = BasiGX.util.ConfigParser.createLayer(clusterLayerConf);
+            mapComponent.getMap().addLayer(layer);
+            plugin = mapComponent.getPlugin('wfscluster');
+        });
+        afterEach(function() {
+            mapComponent.destroy();
+            document.body.removeChild(div);
+            div = null;
+        });
+        it('creates an instance by pluginId', function() {
+            expect(plugin).to.be.a(BasiGX.plugin.WfsCluster);
+        });
+        it('sets the component as member in plugin', function() {
+            expect(plugin.getCmp()).to.be(mapComponent);
+        });
+        it('can prepare clusterlayers', function() {
+            expect(plugin.setUpClusterLayers).withArgs(mapComponent).
+                to.not.throwException();
+            expect(plugin.setUpClusterLayers).to.throwException();
+        });
+        it('sets a defaut cluster style on the layer', function() {
+            plugin.setUpClusterLayers(mapComponent);
+            expect(layer.getStyle()).to.be.a("function");
+            expect(layer.getStyle()).to.eql(plugin.clusterStyleFuntion);
+        });
+        it('registers and calls a visibilityListener on the layer', function() {
+
+            var loadCount = 0;
+            layer.setVisible(false);
+
+            plugin.loadClusterFeatures = function() {
+                loadCount++;
+            };
+            plugin.setUpClusterLayers(mapComponent);
+            layer.setVisible(true);
+            expect(loadCount).to.not.be(0);
+        });
+
+        //TODO: find a way to trigger the moveend event, setCenter wont work
+//        it('registers and calls a moveend listener on map', function() {
+//            var loadCount = 0;
+//
+//            plugin.loadClusterFeatures = function() {
+//                loadCount++;
+//            };
+//            plugin.setUpClusterLayers(mapComponent);
+//            mapComponent.getMap().getView().setCenter([22,3]);
+//            expect(loadCount).to.not.be(0);
+//
+//        });
     });
 });
--- test/spec/util/Application.test.js
+++ test/spec/util/Application.test.js
@@ -6,4 +6,49 @@ describe('BasiGX.util.Application', function() {
             expect(BasiGX.util.Application).to.not.be(undefined);
         });
     });
+    describe('Usage of static Methods', function() {
+        var div;
+        var mapComponent;
+        beforeEach(function() {
+            div = document.createElement('div');
+            document.body.appendChild(div);
+            mapComponent = Ext.create('BasiGX.view.component.Map', {
+                map: new ol.Map({
+                    target: div,
+                    view: new ol.View({
+                        center: [0, 0]
+                    })
+                })
+            });
+        });
+        afterEach(function() {
+            document.body.removeChild(div);
+            div = null;
+            mapComponent.destroy();
+        });
+
+        it('does not throw on getAppContext call', function() {
+            expect(BasiGX.util.Application.getAppContext).
+            to.not.throwException();
+        });
+        it('returns as expected on getAppContext call', function() {
+            var retVal = BasiGX.util.Application.getAppContext();
+            expect(retVal).to.be(null);
+
+            mapComponent.appContext = {};
+            mapComponent.appContext.data = {};
+            mapComponent.appContext.data.merge = {};
+
+            var retVal2 = BasiGX.util.Application.getAppContext();
+            expect(retVal2).to.be.an("object");
+        });
+        it('does not throw on getRoute method call', function() {
+            expect(BasiGX.util.Application.getRoute).
+                to.not.throwException();
+        });
+        it('returns route on getRoute method call', function() {
+            var retVal = BasiGX.util.Application.getRoute();
+            expect(retVal).to.be.a("string");
+        });
+    });
 });
