2c09c1b572e4a427e22bdd2d7e9c36d2be214e0c
Johannes Weskamm <weskamm@terrestris.de>
2016-01-15 15:50:23 +0100

    Added header and documentation

--- src/util/CSRF.js
+++ src/util/CSRF.js
@@ -1,8 +1,33 @@
+/* Copyright (c) 2016 terrestris GmbH & Co. KG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ *
+ * CSRF Util
+ *
+ * Some methods to access the csrf-token information served by spring security
+ *
+ * @class BasiGX.util.CSRF
+ */
 Ext.define('BasiGX.util.CSRF', {
 
     statics: {
         /**
         * Get the CSRF token value.
+        * 
+        * @return {String} - the key value, e.g. "741a3b1-221f-4d1d-..."
         */
         getValue: function() {
             return Ext.DomQuery.select('meta[name=_csrf]')[0].content;
@@ -10,6 +35,8 @@ Ext.define('BasiGX.util.CSRF', {
 
         /**
         * Get the CSRF token key.
+        * 
+        * @return {String} - the key string, e.g. "X-CSRF-TOKEN"
         */
         getKey: function() {
             return Ext.DomQuery.select('meta[name=_csrf_header]')[0].content;
@@ -17,6 +44,8 @@ Ext.define('BasiGX.util.CSRF', {
 
         /**
         * Get the full CSRF token object.
+        * 
+        * @return {Object} header - the header containing the csrf key and value
         */
         getHeader: function() {
             var me = this,
