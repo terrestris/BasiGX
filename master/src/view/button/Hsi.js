2739566f57dd3dd977d88d9896cdc73542f42ef2
Johannes Weskamm <weskamm@terrestris.de>
2016-05-24 12:58:15 +0000

    Update resources on gh-pages branch
    This commit was autogenerated by the 'update-gh-pages.sh' script.

--- master/src/view/button/Hsi.js
+++ master/src/view/button/Hsi.js
@@ -42,7 +42,6 @@ Ext.define("BasiGX.view.button.Hsi", {
      *
      */
     bind: {
-        tooltip: '{tooltip}',
         text: '{text}'
     },
 
@@ -83,6 +82,13 @@ Ext.define("BasiGX.view.button.Hsi", {
      */
     constructor: function(config) {
         this.callParent([config]);
+
+        if (this.setTooltip) {
+            var bind = this.config.bind;
+            bind.tooltip = this.getViewModel().get('tooltip');
+            this.setBind(bind);
+        }
+
         this.toggleButton();
     },
 
