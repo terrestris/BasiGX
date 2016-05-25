f1c948585b589233174d248569a66d98f29ca927
arieger <rieger@terrestris.de>
2016-05-25 12:29:02 +0200

    license and copyright terms

--- src/view/form/field/GazetteerCombo.js
+++ src/view/form/field/GazetteerCombo.js
@@ -1,3 +1,24 @@
+/* Copyright (c) 2016 terrestris GmbH & Co. KG
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Gazetteer combo used to search in the glorious dataset of OSM.
+ *
+ * @class BasiGX.view.form.field.GazetteerCombo
+ *
+ */
 Ext.define('BasiGX.view.form.field.GazetteerCombo',{
     extend: 'Ext.form.field.ComboBox',
 
