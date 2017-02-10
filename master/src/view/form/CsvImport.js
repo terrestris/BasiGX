cb696ff01e308b071fd8f999569c267bcc2a3bf3
Marc Jansen <jansen@terrestris.de>
2017-02-10 10:55:56 +0000

    Update resources on gh-pages branch
    This commit was autogenerated by the 'update-gh-pages.sh' script.

--- master/src/view/form/CsvImport.js
+++ master/src/view/form/CsvImport.js
@@ -1,5 +1,5 @@
 /*global FileReader*/
-/* Copyright (c) 2015 terrestris GmbH & Co. KG
+/* Copyright (c) 2015-present terrestris GmbH & Co. KG
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- master/src/view/panel/Menu.js
+++ master/src/view/panel/Menu.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015 terrestris GmbH & Co. KG
+/* Copyright (c) 2015-present terrestris GmbH & Co. KG
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
