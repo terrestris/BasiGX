18a6a2a1b97179384bca7ca006af1508edbad3eb
Marc Jansen <jansen@terrestris.de>
2017-09-01 12:38:23 +0200

    Address review comments

--- src/util/Caching.js
+++ src/util/Caching.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015-present terrestris GmbH & Co. KG
+/* Copyright (c) 2017-present terrestris GmbH & Co. KG
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@ Ext.define('BasiGX.util.Caching', {
     ],
 
     statics: {
+
         /**
          * Call this function from the console (in one of your applications)
          * and it returns config information that may be used to quickly setup a
@@ -35,50 +36,101 @@ Ext.define('BasiGX.util.Caching', {
          * Note: If the extent is not passed to this function, the ol.view of
          * the ol.map should have explicitly set an 'extent' property!
          *
-         * @param {Number} tileSize The tile size in pixels
-         * @param {Array} extent Optional: The extent to use for the gridSet
+         * @param {ol.View} [view] The OpenLayers map view. If not passed it
+         *     will be guessed.
+         * @param {Array<Number>} [tileSize] The tile size in pixels. If not
+         *     passed we'll assume a 256×256 pixel tile size.
+         * @param {Array} [extent] The extent to use for the gridSet. If not
+         *     passed, we'll take the validity extent of the projection of the
+         *     view.
          * @return {Object} An object with information to quickly setup a GWC
          *     gridset in GeoServer.
          */
-        getGeoWebCacheConfig: function(tileSize, extent) {
-            var me = this;
-            var map = BasiGX.util.Map.getMapComponent().getMap();
-            var mapView = map.getView();
-
-            if (Ext.isEmpty(tileSize)) {
-                Ext.Logger.error('Tile size must be provided!');
+        getGeoWebCacheConfig: function(view, tileSize, extent) {
+            var staticMe = BasiGX.util.Caching;
+            var projection;
+            view = staticMe.sanitizeView(view);
+            if (!view) {
+                Ext.Logger.warn('Failed to guess the ol.View');
                 return;
             }
-
-            if (Ext.isEmpty(extent)) {
-                // if extent has not been passed: try to get it from the ol.view
-                extent = mapView.get('extent');
-
-                if (Ext.isEmpty(extent)) {
-                    Ext.Logger.warn('Extent should be set explicitly ' +
-                        'on the ol.view of the map, if it is not passed ' +
-                        'to this function.');
-                }
+            projection = view.getProjection();
+            tileSize = staticMe.sanitizeTileSize(tileSize);
+            extent = staticMe.sanitizeExtent(extent, view);
+            if (!extent) {
+                Ext.Logger.warn('Failed to determine an extent');
+                return;
             }
-
-            if (!Ext.isEmpty(extent) && !Ext.isArray(extent)) {
-                Ext.Logger.warn('Extent should be an array!');
+            var resolutions = view.getResolutions();
+            var maxResolution = view.getMaxResolution();
+            if (!maxResolution && resolutions) {
+                maxResolution = Ext.Array.max(resolutions);
             }
-
-            var projection = mapView.getProjection().getCode();
-            var resolutions = mapView.getResolutions();
-            var maxResolution = Ext.Array.max(resolutions);
-
-            var tileOrigin = me.getTileOrigin(tileSize, maxResolution, extent);
-
+            var tileOrigin = staticMe.getTileOrigin(
+                tileSize, maxResolution, extent
+            );
             var gwcConfig = {
-                projection: projection,
+                projection: projection.getCode(),
                 resolutions: resolutions,
+                maxResolution: maxResolution,
                 extent: extent,
                 tileOrigin: tileOrigin
             };
 
             return gwcConfig;
+        },
+
+        /**
+         * Computes an extent that completely covers the given extent
+         * for tiles of maxResolution and with the specified tileSize.
+         *
+         *     +------------------+
+         *     |                  | computed extent
+         *     |----------------+ |
+         *     |                | |
+         *     |     passed     | |
+         *     |     extent     | |
+         *     |                | |
+         *     +----------------+-+
+         *
+         * @param {Number|Array<Number>} tileSize The tile size in pixels. Can
+         *     be passed as number or as array of two numbers.
+         * @param {Number} maxResolution The max resolution of the view.
+         * @param {Array} extent The extent to use for the gridset.
+         * @return {Array} The array with two elements representing the
+         *     tileOrigin coordinate (the upper left coordinate of the
+         *     covering extent).
+         */
+        getExtentOfCoveringTileGrid: function(tileSize, maxResolution, extent) {
+            var staticMe = BasiGX.util.Caching;
+            tileSize = staticMe.sanitizeTileSize(tileSize);
+            extent = staticMe.sanitizeExtent(extent);
+            if (!tileSize || !maxResolution || !extent) {
+                Ext.Logger.warn('Tile size, max resolution and extent ' +
+                    'must be provided');
+                return;
+            }
+
+            var minX = extent[0];
+            var minY = extent[1];
+            var extentWidth = ol.extent.getWidth(extent);
+            var extentHeight = ol.extent.getHeight(extent);
+
+            // width/height of one tile at max resolution in the
+            // unit of the underlying projection
+            var tileWidth = tileSize[1] * maxResolution;
+            var tileHeight = tileSize[1] * maxResolution;
+
+            // the minimum number of tiles that are required to fully
+            // cover the given extent in x- or y-direction
+            var minNumTilesX = Math.ceil(extentWidth / tileWidth);
+            var minNumTilesY = Math.ceil(extentHeight / tileHeight);
+
+            // finally compute the max x and y values
+            var maxX = minX + (minNumTilesX * tileWidth);
+            var maxY = minY + (minNumTilesY * tileHeight);
+
+            return [minX, minY, maxX, maxY];
         },
 
         /**
@@ -88,42 +140,134 @@ Ext.define('BasiGX.util.Caching', {
          * match a GWC gridset configured in a GeoServer. GetMap requests
          * should use the parameter tiled=true to make use of the cache.
          *
-         * @param {Number} tileSize The tile size in pixels
-         * @param {Number} maxResolution The max resolution
-         * @param {Array} extent The extent to use for the gridSet
+         *     Top-left
+         *     X------------------+
+         *     |                  |
+         *     |----------------+ |
+         *     |                | |
+         *     |     passed     | |
+         *     |     extent     | |
+         *     |                | |
+         *     +----------------+-+
+         *
+         * @param {Number|Array<Number>} tileSize The tile size in pixels. Can
+         *     be passed as number or as array of two numbers.
+         * @param {Number} maxResolution The max resolution of the view.
+         * @param {Array} extent The extent to use for the gridset.
          * @return {Array} The array with two elements representing the
-         *     tileOrigin coordinate.
+         *     tileOrigin coordinate (the upper left coordinate of the
+         *     covering extent).
          */
         getTileOrigin: function(tileSize, maxResolution, extent) {
-            if (Ext.isEmpty(tileSize) || Ext.isEmpty(maxResolution) ||
-                Ext.isEmpty(extent)) {
-                Ext.Logger.error('Tile size, max resolution and extent ' +
-                    'must be provided!');
+            var staticMe = BasiGX.util.Caching;
+            var coveringExtent = staticMe.getExtentOfCoveringTileGrid(
+                tileSize, maxResolution, extent
+            );
+            if (!coveringExtent) {
+                Ext.Logger.warn(
+                    'Failed to determine covering extent and origin'
+                );
+                return;
             }
+            return ol.extent.getTopLeft(coveringExtent);
+        },
 
-            if (!Ext.isArray(extent)) {
-                Ext.Logger.error('Extent must be an array!');
+        /**
+         * Sanitizes the passed view. If a falsy value is passed or the passed
+         * view is not an instance of `ol.View`, guess the map component on the
+         * page and return the view of its map.
+         *
+         * @param {ol.View} [view] The OpenLayers map view. If not passed it
+         *     will be guessed.
+         * @return {ol.View} A view or undefined if the view could not be
+         *     sanitized.
+         * @private
+         */
+        sanitizeView: function(view) {
+            if (!view || !(view instanceof ol.View)) {
+                Ext.Logger.info('No or unexpected view passed,' +
+                    ' guessing ol.View now');
+                var mapComp = BasiGX.util.Map.getMapComponent();
+                var map = mapComp && mapComp.getMap();
+                view = map && map.getView();
+                if (!view) {
+                    Ext.Logger.warn('Failed to guess the ol.View');
+                    return;
+                }
             }
+            return view;
+        },
 
-            var minX = extent[0];
-            var minY = extent[1];
-            var maxY = extent[3];
+        /**
+         * Sanitizes the passed tile size. If a number is passed, this method
+         * will assume a rectangular tile and return an array of `[in, in]`. If
+         * an array of two numbers is passed, the tile size will be returned
+         * unchanged. Anything else will result in the default tile size of
+         * `[256, 256]` being returned.
+         *
+         * @param {Number|Array<Number>} [tileSize] The tile size in pixels. Can
+         *     be passed as number or as array of two numbers.
+         * @return {Array<Number>} The sanitized tile size as array of two
+         *     numbers or `[256, 256]` if we received something unexpected.
+         * @private
+         */
+        sanitizeTileSize: function(tileSize) {
+            var msg;
+            if (Ext.isNumber(tileSize)) {
+                msg = 'Parameter tileSize passed as number,' +
+                    ' assuming rectangular tilesize of [{0}, {0}] now';
+                Ext.Logger.info(Ext.String.format(msg, tileSize));
+                tileSize = [tileSize, tileSize];
+            }
+            if (!Ext.isArray(tileSize) ||
+                    !Ext.isNumber(tileSize[0]) || !Ext.isNumber(tileSize[1])) {
+                Ext.Logger.info('No or unexpected tileSize passed,' +
+                    ' defaulting to [256, 256] now');
+                tileSize = [256, 256];
+            }
+            return tileSize;
+        },
 
-            // height of the extent
-            var extentHeight = maxY - minY;
-
-            // height of one tile at max resolution in the
-            // unit of the underlying projection
-            var tileHeight = tileSize * maxResolution;
-
-            // the minimum number of tiles that are required to fully
-            // fit the given extent (from bottom to top)
-            var minNrOfTiles = Math.ceil(extentHeight / tileHeight);
-
-            // finally compute the tile origin
-            var tileOriginY = minY + (minNrOfTiles * tileHeight);
-
-            return [minX, tileOriginY];
+        /**
+         * Sanitizes the passed extent, potentially taking into account the
+         * passed `ol.View`. A valid extent (left, bottom, right, top) will
+         * be returned unchanged. When the passed extent is falsy, the view
+         * will be queried for a property `extent`, and the checking of the
+         * validity will use this extent. If we consider the extent invalid,
+         * the view will be asked to provide us the validity extent of its
+         * projection, and we'll use that.
+         *
+         * @param {Array<Number>} [extent] The extent we want to sanitize.
+         * @param {ol.View} [view] Will be used to in case the passed extent is
+         *     falsy or otherwise unexpected. First we check if there is a
+         *     property 'extent' on the view, and finally we'll return the
+         *     validity extent from the projection of the view. Please note that
+         *     this might be `undefined` as well.
+         * @return {Array<Number>} The normalized extent or `undefined`.
+         * @private
+         */
+        sanitizeExtent: function(extent, view) {
+            // some views might have this explicitly set
+            extent = extent || (view && view.get('extent'));
+            if (!Ext.isArray(extent) ||
+                    !Ext.isNumber(extent[0]) || !Ext.isNumber(extent[1]) ||
+                    !Ext.isNumber(extent[2]) || !Ext.isNumber(extent[3]) ||
+                    ol.extent.getWidth(extent) <= 0 ||
+                    ol.extent.getHeight(extent) <= 0) {
+                var msg = 'No or unexpected extent passed, ';
+                var projection = view && view.getProjection();
+                if (projection) {
+                    msg += 'defaulting to validity extent of view projection';
+                    Ext.Logger.info(msg);
+                    extent = projection.getExtent();
+                } else {
+                    msg += 'and view cannot be used to derive one';
+                    Ext.Logger.info(msg);
+                    return;
+                }
+            }
+            return extent;
         }
+
     }
 });
---
+++ test/spec/util/Caching.test.js
@@ -0,0 +1,253 @@
+Ext.Loader.syncRequire(['BasiGX.util.Caching']);
+
+describe('BasiGX.util.Caching', function() {
+    // ----------------------
+    // Basic setup
+    // ----------------------
+
+    // set up a custom project for OpenLayers
+    var utmProj = new ol.proj.Projection({
+        code: 'EPSG:25832',
+        extent: [-1878007.03, 3932282.86, 831544.53, 9437501.55],
+        units: 'm'
+    });
+    ol.proj.addProjection(utmProj);
+
+    var testObjs;
+    var origExtLogger;
+    beforeEach(function() {
+        testObjs = TestUtil.setupTestObjects();
+        origExtLogger = Ext.Logger;
+        // turn off logging during these tests
+        Ext.Logger = {info: Ext.emptyFn, warn: Ext.emptyFn};
+    });
+    afterEach(function() {
+        TestUtil.teardownTestObjects(testObjs);
+        // restore old logging behaviour
+        Ext.Logger = origExtLogger;
+    });
+
+    var clazz = BasiGX.util.Caching;
+
+    // ----------------------
+    // Tests
+    // ----------------------
+    describe('Basics', function() {
+        it('is defined', function() {
+            expect(clazz).to.not.be(undefined);
+        });
+    });
+
+    describe('Static methods', function() {
+
+        describe('#getGeoWebCacheConfig', function() {
+            it('is a defined function', function() {
+                expect(clazz.getGeoWebCacheConfig).to.be.a('function');
+            });
+            it('can be called without parameters', function() {
+                expect(clazz.getGeoWebCacheConfig).to.not.throwException();
+            });
+            it('returns a valid cfg, guessing everything', function() {
+                var view = testObjs.map.getView();
+                var extent = view.getProjection().getExtent();
+                var got = clazz.getGeoWebCacheConfig(); // no params passed
+                expect(got.projection).to.be(view.getProjection().getCode());
+                expect(got.resolutions).to.eql(view.getResolutions());
+                expect(got.maxResolution).to.eql(view.getMaxResolution());
+                expect(got.extent).to.eql(extent);
+                expect(got.tileOrigin).to.eql([extent[0], extent[3]]);
+            });
+            it('returns a valid cfg, explicitly passing stuff', function() {
+                var view = testObjs.map.getView();
+                var tileSize = [256, 256];
+                var extent = view.getProjection().getExtent();
+                // explictly pass them
+                var got = clazz.getGeoWebCacheConfig(view, tileSize, extent);
+                expect(got.projection).to.be(view.getProjection().getCode());
+                expect(got.resolutions).to.eql(view.getResolutions());
+                expect(got.maxResolution).to.eql(view.getMaxResolution());
+                expect(got.extent).to.eql(extent);
+                expect(got.tileOrigin).to.eql([extent[0], extent[3]]);
+            });
+            it('returns a valid cfg, tileSize as number', function() {
+                var view = testObjs.map.getView();
+                var tileSize = 256; // a single number
+                var extent = view.getProjection().getExtent();
+                var got = clazz.getGeoWebCacheConfig(view, tileSize, extent);
+                expect(got.projection).to.be(view.getProjection().getCode());
+                expect(got.resolutions).to.eql(view.getResolutions());
+                expect(got.maxResolution).to.eql(view.getMaxResolution());
+                expect(got.extent).to.eql(extent);
+                expect(got.tileOrigin).to.eql([extent[0], extent[3]]);
+            });
+            it('returns a valid cfg, for alternative tilesizes', function() {
+                var view = testObjs.map.getView();
+                var tileSize = [512, 128]; // irregular tile
+                var extent = view.getProjection().getExtent();
+                var got = clazz.getGeoWebCacheConfig(view, tileSize, extent);
+                expect(got.projection).to.be(view.getProjection().getCode());
+                expect(got.resolutions).to.eql(view.getResolutions());
+                expect(got.maxResolution).to.eql(view.getMaxResolution());
+                expect(got.extent).to.eql(extent);
+                expect(got.tileOrigin).to.eql([extent[0], extent[3]]);
+            });
+            it('returns a valid cfg, for alternative extents', function() {
+                var view = testObjs.map.getView();
+                var tileSize = [128, 128]; // reduce so that tiles fit in extent
+                var extent = view.getProjection().getExtent();
+                extent[2] = 0;
+                extent[3] = 0; // only a quarter of the world
+                var got = clazz.getGeoWebCacheConfig(view, tileSize, extent);
+                expect(got.projection).to.be(view.getProjection().getCode());
+                expect(got.resolutions).to.eql(view.getResolutions());
+                expect(got.maxResolution).to.eql(view.getMaxResolution());
+                expect(got.extent).to.eql(extent);
+                expect(got.tileOrigin).to.eql([extent[0], extent[3]]);
+            });
+            it('works for real world values, EPSG:25832', function() {
+                var view = new ol.View({
+                    projection: 'EPSG:25832',
+                    resolutions: [
+                        1120.002240004, 560.0011200022, 280.0005600011,
+                        140.0002800006, 70.0001400003, 28.0000560001,
+                        14.0000280001, 7.000014, 2.8000056, 1.4000028,
+                        0.7000014, 0.28000056, 0.14000028
+                    ]
+                });
+                var tileSize = [256, 256];
+                var extent = [237000, 5207000, 960000, 6150000];
+                var got = clazz.getGeoWebCacheConfig(view, tileSize, extent);
+                // From GWC preview page
+                var expectedOrigin = [237000, 6353882.293764096];
+                expect(got.projection).to.be(view.getProjection().getCode());
+                expect(got.resolutions).to.eql(view.getResolutions());
+                expect(got.maxResolution).to.eql(view.getMaxResolution());
+                expect(got.extent).to.eql(extent);
+                expect(got.tileOrigin).to.eql(expectedOrigin);
+            });
+
+            it('works for real world values, EPSG:4326', function() {
+                var view = new ol.View({
+                    projection: 'EPSG:4326',
+                    resolutions: [
+                        0.010072426874075599, 0.0050362134370377995,
+                        0.0025181067185188998, 0.0012590533592594499,
+                        0.0006295266796297249, 0.00025181067185189,
+                        0.000125905335925945, 0.0000629526679629725,
+                        0.000025181067185188997, 0.000012590533592594498,
+                        0.000006295266796297249, 0.0000025181067185188997,
+                        0.0000012590533592594498
+                    ]
+                });
+                var tileSize = [256, 256];
+                var extent = [
+                    5.542797987676652, 46.96430005289825,
+                    16.248509129820956, 55.28072087437724
+                ];
+                var got = clazz.getGeoWebCacheConfig(view, tileSize, extent);
+                // From GWC preview page
+                var expectedOrigin = [5.542797987676652, 57.278465171951666];
+                expect(got.projection).to.be(view.getProjection().getCode());
+                expect(got.resolutions).to.eql(view.getResolutions());
+                expect(got.maxResolution).to.eql(view.getMaxResolution());
+                expect(got.extent).to.eql(extent);
+                expect(got.tileOrigin).to.eql(expectedOrigin);
+            });
+        });
+
+        describe('#getExtentOfCoveringTileGrid', function() {
+            it('is a defined function', function() {
+                expect(clazz.getExtentOfCoveringTileGrid).to.be.a('function');
+            });
+            it('can be called without parameters', function() {
+                expect(clazz.getExtentOfCoveringTileGrid)
+                    .to.not.throwException();
+            });
+            it('works for real world values, EPSG:25832', function() {
+                var maxResolution = 1120.002240004;
+                var tileSize = [256, 256];
+                var extent = [237000, 5207000, 960000, 6150000];
+
+                // Derived from GWC preview page
+                var expectedExtent = [
+                    237000, 5207000,
+                    1097161.720323072, 6353882.293764096
+                ];
+
+                var got = clazz.getExtentOfCoveringTileGrid(
+                    tileSize, maxResolution, extent
+                );
+
+                expect(got).to.eql(expectedExtent);
+                expect(
+                    ol.extent.containsExtent(expectedExtent, extent)
+                ).to.be.ok();
+            });
+
+            it('works for real world values, EPSG:4326', function() {
+                var maxResolution = 0.010072426874075599;
+                var tileSize = [256, 256];
+                var extent = [
+                    5.542797987676652, 46.96430005289825,
+                    16.248509129820956, 55.28072087437724
+                ];
+
+                // Derived from GWC preview page
+                var expectedExtent = [
+                    5.542797987676652, 46.96430005289825,
+                    18.43550438649342, 57.278465171951666
+                ];
+
+                var got = clazz.getExtentOfCoveringTileGrid(
+                    tileSize, maxResolution, extent
+                );
+
+                expect(got).to.eql(expectedExtent);
+                expect(
+                    ol.extent.containsExtent(expectedExtent, extent)
+                ).to.be.ok();
+            });
+        });
+
+        describe('#getTileOrigin', function() {
+            it('is a defined function', function() {
+                expect(clazz.getTileOrigin).to.be.a('function');
+            });
+            it('can be called without parameters', function() {
+                expect(clazz.getTileOrigin).to.not.throwException();
+            });
+            it('works for real world values, EPSG:25832', function() {
+                var maxResolution = 1120.002240004;
+                var tileSize = [256, 256];
+                var extent = [237000, 5207000, 960000, 6150000];
+
+                // Derived from GWC preview page
+                var expectedOrigin = [237000, 6353882.293764096];
+
+                var got = clazz.getTileOrigin(
+                    tileSize, maxResolution, extent
+                );
+
+                expect(got).to.eql(expectedOrigin);
+            });
+
+            it('works for real world values, EPSG:4326', function() {
+                var maxResolution = 0.010072426874075599;
+                var tileSize = [256, 256];
+                var extent = [
+                    5.542797987676652, 46.96430005289825,
+                    16.248509129820956, 55.28072087437724
+                ];
+
+                // Derived from GWC preview page
+                var expectedOrigin = [5.542797987676652, 57.278465171951666];
+
+                var got = clazz.getTileOrigin(
+                    tileSize, maxResolution, extent
+                );
+
+                expect(got).to.eql(expectedOrigin);
+            });
+        });
+    });
+});
