201448e4eb115c1d66f859fce23131880699d4a5
Daniel Koch <koch@terrestris.de>
2018-01-09 07:55:12 +0000

    Update resources on gh-pages branch
    This commit was autogenerated by the 'update-gh-pages.sh' script
    and it reflects the state of the master branch at revision
    74ad784683706fba4b1dd749cc4af0f76846e90c (by Daniel Koch).

--- master/src/util/SLD.js
+++ master/src/util/SLD.js
@@ -41,7 +41,7 @@ Ext.define('BasiGX.util.SLD', {
         ],
         DEFAULT_STROKE_OPACITY: '1',
         DEFAULT_STROKE_COLOR: '#000000',
-        DEFAULT_STROKE_WIDTH: '0',
+        DEFAULT_STROKE_WIDTH: '1',
 
         DEFAULT_FILL_COLOR: '#FF0000',
         DEFAULT_FILL_OPACITY: '1',
