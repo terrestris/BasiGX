47800ce50e24ed31d566b4dea61aa6ed13a4b597
Andreas Schmitz <schmitz@occamlabs.de>
2018-05-17 09:20:18 +0200

    add the full split parameter to the intersection op (#318)
    This adds two new parameters to the intersection operation:
    
    `tolerance`: used to filter out resulting polygons smaller that this
    and to compare polygons for equality.
    
    `fullSplit`: used to obtain a list of all geometry parts resulting
    after doing an intersection between possibly multiple polygons.

--- src/view/button/SpatialOperatorIntersect.js
+++ src/view/button/SpatialOperatorIntersect.js
@@ -51,9 +51,28 @@ Ext.define('BasiGX.view.button.SpatialOperatorIntersect', {
         /**
          * the number of features which are allowed to process together.
          * Union may use a high number without problems,
-         * intersect will be restricted to 2
+         * intersect will be restricted to 2. With fullSplit set to true, the
+         * number is not limited.
          */
-        maxAllowedFeaturesForOperation: 2
+        maxAllowedFeaturesForOperation: 2,
+
+        /**
+         * If set to true, the operator will calculate all resulting parts of
+         * the intersection, including the parts that do not intersect with
+         * other geometries. Default is false.
+         * @type {Boolean}
+         */
+        fullSplit: false,
+
+        /**
+         * The tolerance to use when comparing geometries during difference and
+         * intersection calculation. Set this to a sane value in order to avoid
+         * getting overlapping/duplicate geometries. This is also used to filter
+         * out geometries smaller that this value in size (this happens a lot if
+         * multiple geometries are involved).
+         * @type {Number}
+         */
+        tolerance: 0
     },
 
     toggleGroup: 'draw',
@@ -80,12 +99,63 @@ Ext.define('BasiGX.view.button.SpatialOperatorIntersect', {
         var parser = new jsts.io.OL3Parser();
         var parsedGeometries = [];
         var intersectionGeom;
-        var newFeature = null;
 
         Ext.each(featureArray, function(feat) {
             parsedGeometries.push(parser.read(feat.getGeometry()));
         });
 
+        if (this.getFullSplit()) {
+            intersectionGeom = this.calculateFullSplit(parsedGeometries);
+        } else {
+            intersectionGeom = this.calculateSingleIntersection(
+                parsedGeometries);
+        }
+
+        if (intersectionGeom) {
+            if (!Ext.isArray(intersectionGeom)) {
+                intersectionGeom = [intersectionGeom];
+            }
+            var newFeatures = [];
+            Ext.each(intersectionGeom, function(geom) {
+                var result = parser.write(geom);
+                newFeatures.push(new ol.Feature({geometry: result}));
+            });
+            return newFeatures;
+        }
+
+        return false;
+    },
+
+    /**
+     * Normalizes the geometry, then adds it to the list if not contained yet.
+     * This is a rather expensive operation, use only when necessary!
+     * @param  {jsts.geom.Geometry} geom the jsts geometry
+     * @param  {Array} list the list to possibly add the geometry to
+     */
+    pushIfNotEqual: function(geom, list) {
+        var me = this;
+        var foundEqual = false;
+        geom.normalize();
+
+        Ext.each(list, function(other) {
+            if (other.equalsExact(geom, me.getTolerance())) {
+                foundEqual = true;
+                return false;
+            }
+        });
+        if (!foundEqual) {
+            list.push(geom);
+        }
+    },
+
+    /**
+     * Calculates just the intersection of some geometries.
+     * @param  {jsts.geom.Geometry} parsedGeometries the jsts geometries
+     * @return {jsts.geom.Geometry|Boolean} the intersection, or false, if it
+     * could not be calculated or the resulting geometry is too small or invalid
+     */
+    calculateSingleIntersection: function(parsedGeometries) {
+        var intersectionGeom;
         for (var i = 0; i < parsedGeometries.length; i++) {
             if (i === 0) {
                 intersectionGeom = parsedGeometries[i];
@@ -95,11 +165,112 @@ Ext.define('BasiGX.view.button.SpatialOperatorIntersect', {
             }
         }
         if (!intersectionGeom.isValid() || intersectionGeom.isEmpty() ||
-            intersectionGeom.getArea() === 0) {
+            (intersectionGeom.getGeometryType() !== 'Polygon' &&
+            intersectionGeom.getGeometryType() !== 'MultiPolygon') ||
+            intersectionGeom.getArea() <= this.getTolerance()) {
             return false;
         }
-        var result = parser.write(intersectionGeom);
-        newFeature = new ol.Feature({geometry: result});
-        return newFeature;
+        return intersectionGeom;
+    },
+
+    /**
+     * Calculates the difference polygons for a set of polygons. The difference
+     * polygons are the parts of the polygons that do not intersect with any
+     * others.
+     * @param  {jsts.geom.Geometry[]} geometries the jsts geometries
+     * @return {jsts.geom.Geometry[]} the set of difference polygons
+     */
+    calculateDifferences: function(geometries) {
+        var me = this;
+        var results = [];
+
+        Ext.each(geometries, function(geometry) {
+            var difference = geometry;
+            Ext.each(geometries, function(other) {
+                try {
+                    if (geometry !== other) {
+                        difference = difference.difference(other);
+                    }
+                } catch (e) {
+                    // ignore topology exceptions for now
+                }
+            });
+            if (difference.isEmpty() || !difference.isValid() ||
+                (difference.getGeometryType() !== 'Polygon' &&
+                difference.getGeometryType() !== 'MultiPolygon') ||
+                difference.getArea() <= me.getTolerance()) {
+                return;
+            }
+            me.pushIfNotEqual(difference, results);
+        });
+
+        return results;
+    },
+
+    /**
+     * Calculates the intersection geometries for a given set of polygons. Note
+     * that this may calculate overlapping polygons if multiple polygons are
+     * involved.
+     * @param  {jsts.geom.Geometry[]} geometries the jsts geometries
+     * @return {jsts.geom.Geometry[]} the intersection polygon list
+     */
+    calculateIntersections: function(geometries) {
+        var me = this;
+        var results = [];
+
+        Ext.each(geometries, function(geometry) {
+            Ext.each(geometries, function(other) {
+                if (geometry === other) {
+                    return;
+                }
+                try {
+                    var intersection = geometry.intersection(other);
+                    if (intersection.isEmpty() || !intersection.isValid() ||
+                        (intersection.getGeometryType() !== 'Polygon' &&
+                        intersection.getGeometryType() !== 'MultiPolygon') ||
+                        intersection.getArea() <= me.getTolerance()) {
+                        return;
+                    }
+                    me.pushIfNotEqual(intersection, results);
+                } catch (e) {
+                    // ignore topology exceptions for now
+                }
+            });
+        });
+
+        return results;
+    },
+
+    /**
+     * Calculates the full split polygons when intersecting a group of polygons.
+     * It works by repeatedly calculating the difference polygons, adding them
+     * to the list of results, and then calculating the intersections. If no
+     * differences were found, they're added to the results list and we're done.
+     * If differences were found, repeat the process with the differences as new
+     * input.
+     * @param  {jsts.geom.Geometry[]} geometries the list of input polygons
+     * @return {jsts.geom.Geometry[]} the list of the resulting split geometries
+     */
+    calculateFullSplit: function(geometries) {
+        var me = this;
+        var results = [];
+        var intersections = geometries;
+
+        while (intersections.length > 0) {
+            var differences = this.calculateDifferences(intersections);
+            Ext.each(differences, function(geom) {
+                me.pushIfNotEqual(geom, results);
+            });
+            intersections = this.calculateIntersections(intersections);
+            if (differences.length === 0) {
+                Ext.each(intersections, function(geom) {
+                    me.pushIfNotEqual(geom, results);
+                });
+                intersections = [];
+            }
+        }
+
+        return results;
     }
+
 });
