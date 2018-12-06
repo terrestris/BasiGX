c839e26f084ab730f36d3eebb4fb179f5dd30be8
Andreas Schmitz <schmitz@occamlabs.de>
2018-12-06 12:08:04 +0000

    Update resources on gh-pages branch
    This commit was autogenerated by the 'update-gh-pages.sh' script
    and it reflects the state of the master branch at revision
    1f671858ef4260a1864919a7aef915a04ee29e5c (by Andreas Schmitz).

--- master/src/view/button/DigitizePostit.js
+++ master/src/view/button/DigitizePostit.js
@@ -136,8 +136,14 @@ Ext.define('BasiGX.view.button.DigitizePostit', {
             );
             var imageBaseSrc;
             if (classPath) {
-                imageBaseSrc = classPath.split(
-                    'src/view/button/DigitizePostit.js')[0];
+                var devPath = 'src/view/button/DigitizePostit.js';
+                if (classPath.indexOf(devPath) === -1) {
+                    // we're on a production build, so let's use the
+                    // image from the classic resource folder
+                    imageBaseSrc = 'classic/';
+                } else {
+                    imageBaseSrc = classPath.split(devPath)[0];
+                }
             }
             return imageBaseSrc + 'resources/img/postit.png';
         }
