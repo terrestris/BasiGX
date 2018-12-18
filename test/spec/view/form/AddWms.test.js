e06fa6235ac0f35c20a1da5b249d88f4c4b50835
Marc Jansen <jansen@terrestris.de>
2018-12-18 14:27:48 +0100

    Some more tests

--- test/spec/view/form/AddWms.test.js
+++ test/spec/view/form/AddWms.test.js
@@ -52,10 +52,6 @@ describe('BasiGX.view.form.AddWms', function() {
         });
 
         it('renders the textfield if no wmsBaseUrls', function() {
-            form.destroy();
-            form = Ext.create('BasiGX.view.form.AddWms', {
-                renderTo: Ext.getBody()
-            });
             var textfield = form.down('textfield[name="url"]');
             var combo = form.down('combo[name="urlCombo"]');
             expect(textfield.isVisible()).to.be(true);
@@ -72,6 +68,163 @@ describe('BasiGX.view.form.AddWms', function() {
             var combo = form.down('combo[name="urlCombo"]');
             expect(textfield.isVisible()).to.be(false);
             expect(combo.isVisible()).to.be(true);
+        });
+
+        it('can be configured with a default URL (textfield)', function() {
+            form.destroy();
+            form = Ext.create('BasiGX.view.form.AddWms', {
+                renderTo: Ext.getBody(),
+                defaultUrl: 'Peter'
+            });
+            var textfield = form.down('textfield[name="url"]');
+            expect(textfield.getValue()).to.be('Peter');
+        });
+
+        it('can be configured with a default URL (combo)', function() {
+            form.destroy();
+            form = Ext.create('BasiGX.view.form.AddWms', {
+                renderTo: Ext.getBody(),
+                defaultUrl: 'Kalle'
+            });
+            var combo = form.down('combo[name="urlCombo"]');
+            expect(combo.getValue()).to.be('Kalle');
+        });
+
+        it('has a version selection container by default', function() {
+            var versionContainer = form.down('fieldcontainer');
+            expect(versionContainer).to.be.ok();
+            expect(versionContainer.isVisible()).to.be(true);
+        });
+
+        it('hides version selection, if autoDetectVersion=true', function() {
+            form.destroy();
+            form = Ext.create('BasiGX.view.form.AddWms', {
+                renderTo: Ext.getBody(),
+                autoDetectVersion: true
+            });
+            var versionContainer = form.down('fieldcontainer');
+            expect(versionContainer).to.be.ok();
+            expect(versionContainer.isVisible()).to.be(false);
+        });
+
+    });
+
+    describe('Initialisation', function() {
+        it('creates one parser during initialisation', function() {
+            var form = Ext.create('BasiGX.view.form.AddWms');
+            expect(form.parser).to.be.ok();
+            expect(form.parser).to.be.an(ol.format.WMSCapabilities);
+            form.destroy();
+            // teardown manually, only one test herein
+        });
+    });
+
+    describe('Methods', function() {
+        var form = null;
+        beforeEach(function() {
+            form = Ext.create('BasiGX.view.form.AddWms', {
+                renderTo: Ext.getBody()
+            });
+        });
+        afterEach(function() {
+            if(form) {
+                form.destroy();
+                form = null;
+            }
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns a key for CORS status', function() {
+                var exp = 'msgCorsMisconfiguration';
+                expect(form.responseStatusToErrorMsgKey(0)).to.be(exp);
+                expect(form.responseStatusToErrorMsgKey('0')).to.be(exp);
+                expect(form.getViewModel().get(exp)).to.be.ok();
+            });
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns a key for unauthorized error status', function() {
+                var exp = 'msgUnauthorized';
+                expect(form.responseStatusToErrorMsgKey(401)).to.be(exp);
+                expect(form.responseStatusToErrorMsgKey('401')).to.be(exp);
+                expect(form.getViewModel().get(exp)).to.be.ok();
+            });
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns a key for forbidded error status', function() {
+                var exp = 'msgForbidden';
+                expect(form.responseStatusToErrorMsgKey(403)).to.be(exp);
+                expect(form.responseStatusToErrorMsgKey('403')).to.be(exp);
+                expect(form.getViewModel().get(exp)).to.be.ok();
+            });
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns a key for file not found error status', function() {
+                var exp = 'msgFileNotFound';
+                expect(form.responseStatusToErrorMsgKey(404)).to.be(exp);
+                expect(form.responseStatusToErrorMsgKey('404')).to.be(exp);
+                expect(form.getViewModel().get(exp)).to.be.ok();
+            });
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns a key for too many requests error status', function() {
+                var exp = 'msgTooManyRequests';
+                expect(form.responseStatusToErrorMsgKey(429)).to.be(exp);
+                expect(form.responseStatusToErrorMsgKey('429')).to.be(exp);
+                expect(form.getViewModel().get(exp)).to.be.ok();
+            });
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns a key for sevice unavailable error status', function() {
+                var exp = 'msgServiceUnavailable';
+                expect(form.responseStatusToErrorMsgKey(503)).to.be(exp);
+                expect(form.responseStatusToErrorMsgKey('503')).to.be(exp);
+                expect(form.getViewModel().get(exp)).to.be.ok();
+            });
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns a key for gateway timeout error status', function() {
+                var exp = 'msgGatewayTimeOut';
+                expect(form.responseStatusToErrorMsgKey(504)).to.be(exp);
+                expect(form.responseStatusToErrorMsgKey('504')).to.be(exp);
+                expect(form.getViewModel().get(exp)).to.be.ok();
+            });
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns null for unexpected status', function() {
+                var exp = null;
+                var checks = [
+                    -1, 'humpty', false, NaN, [], {}, function(){}
+                ];
+                Ext.each(checks, function(check) {
+                    expect(form.responseStatusToErrorMsgKey(check)).to.be(exp);
+                    expect(form.responseStatusToErrorMsgKey('' + check)).to.be(exp);
+                });
+            });
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns a generic key for client error status', function() {
+                var exp = 'msgClientError';
+                var checks = [
+                    400, 410, 450, 465, 499
+                ];
+                Ext.each(checks, function(check) {
+                    expect(form.responseStatusToErrorMsgKey(check)).to.be(exp);
+                    expect(form.responseStatusToErrorMsgKey('' + check)).to.be(exp);
+                    expect(form.getViewModel().get(exp)).to.be.ok();
+                });
+            });
+        });
+        describe('responseStatusToErrorMsgKey', function() {
+            it('returns a generic key for server error status', function() {
+                var exp = 'msgServerError';
+                var checks = [
+                    500, 510, 550, 565, 664, 799
+                ];
+                Ext.each(checks, function(check) {
+                    expect(form.responseStatusToErrorMsgKey(check)).to.be(exp);
+                    expect(form.responseStatusToErrorMsgKey('' + check)).to.be(exp);
+                    expect(form.getViewModel().get(exp)).to.be.ok();
+                });
+            });
         });
     });
 });
