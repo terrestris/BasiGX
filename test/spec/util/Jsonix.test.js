e9a66040c0ba2e3ce565f4a4c121885e8e46973b
Nils Bühner <buehner@terrestris.de>
2019-04-17 11:26:21 +0200

    Add basic tests and remove unneeded test

---
+++ test/spec/util/Jsonix.test.js
@@ -0,0 +1,9 @@
+Ext.Loader.syncRequire(['BasiGX.util.Jsonix']);
+
+describe('BasiGX.util.Jsonix', function() {
+    describe('Basics', function() {
+        it('is defined', function() {
+            expect(BasiGX.util.Jsonix).to.not.be(undefined);
+        });
+    });
+});
---
+++ test/spec/util/MouseCoordinates.test.js
@@ -0,0 +1,9 @@
+Ext.Loader.syncRequire(['BasiGX.util.MouseCoordinates']);
+
+describe('BasiGX.util.MouseCoordinates', function() {
+    describe('Basics', function() {
+        it('is defined', function() {
+            expect(BasiGX.util.MouseCoordinates).to.not.be(undefined);
+        });
+    });
+});
