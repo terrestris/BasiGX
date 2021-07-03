674a7a39892bce81b802c31e2541ab78c4b4b88d
sethg <sethg@geographika.co.uk>
2021-07-03 11:20:48 +0200

    Pass geometry rather than coords to ol.sphere.getArea

--- src/view/button/Measure.js
+++ src/view/button/Measure.js
@@ -759,10 +759,8 @@ Ext.define('BasiGX.view.button.Measure', {
 
         if (me.geodesic) {
             var sourceProj = me.map.getView().getProjection();
-            var geom = (polygon.clone().transform(
-                sourceProj, 'EPSG:4326'));
-            var coordinates = geom.getLinearRing(0).getCoordinates();
-            area = Math.abs(ol.sphere.getArea(coordinates));
+            var geom = polygon.clone();
+            area = Math.abs(ol.sphere.getArea(geom, sourceProj));
         } else {
             area = polygon.getArea();
         }
